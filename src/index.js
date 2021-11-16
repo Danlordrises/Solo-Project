7c8c50578d82f9a7173cf2cddbdd4315d9653438
Daniel <dannguyen1191@yahoo.com>
2021-11-15 22:30:45 -0600

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css'
+// DONT FORGET TO IMPORT COMPONENTS HERE
+
+
+
+const Index = () => {
+  return <div>Welcome to React!</div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
